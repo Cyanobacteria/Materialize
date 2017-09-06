8add768d719f9639e2d94c713bf1a2493df94530
cyanobacteria <vodomelkor@gmail.com>
2017-09-06 22:37:39 +0800

    有一個fonts load error 待解決

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require rails-ujs
 //= require jquery
 //= require jquery_ujs
+//= require materialize-sprockets
 //= require jquery.turbolinks
 //= require materialize
 //= require turbolinks
